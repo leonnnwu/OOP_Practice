6990625ed77b71dbf82c66739e0969def0aabf90
leonnnwu <wuliang0306@gmail.com>
2014-05-17 23:31:48 -0700

    Adapter Pattern

---
+++ Adapter/AdapterPatternDemo.java
@@ -0,0 +1,10 @@
+public class AdapterPatternDemo {
+	public static void main(String[] args) {
+		AudioPlayer audioPlayer = new AudioPlayer();
+
+		audioPlayer.play("mp3", "111.mp3");
+		audioPlayer.play("mp4", "222.mp4");
+		audioPlayer.play("vlc", "333.vlc");
+		audioPlayer.play("avi", "444.avi");
+	}
+}
---
+++ Adapter/Mp4Player.java
@@ -0,0 +1,9 @@
+public class Mp4Player implements AdvancedMediaPlayer {
+	public void playVlc(String fileName) {
+		
+	}
+
+	public void playMp4(String fileName) {
+		System.out.println("Playing mp4 file. Name: " + fileName);
+	}
+}
